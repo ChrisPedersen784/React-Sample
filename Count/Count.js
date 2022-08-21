d5099459db862939431e684692c759ea0f595487
Christian Pedersen <76157202+ChrisPedersen784@users.noreply.github.com>
2022-08-21 17:16:38 +0200

    Add files via upload

---
+++ Count/Count.js
@@ -0,0 +1,12 @@
+import React from "react"
+import App from "./App"
+
+
+   export default function Count(props){
+      return (
+        <div className="counter--count">
+                <h1>{props.number}</h1>
+            </div>
+    )   
+    }
+    
