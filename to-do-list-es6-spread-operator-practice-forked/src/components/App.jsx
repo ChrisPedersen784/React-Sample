ac73746be974da0d416952ac1e8a8a80f29f6df7
Christian Pedersen <76157202+ChrisPedersen784@users.noreply.github.com>
2022-08-21 17:55:35 +0200

    Add files via upload

---
+++ to-do-list-es6-spread-operator-practice-forked/src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  //empty array to hold the items
+  const [items, setItems] = useState([]);
+
+  //Event handler to set inputText to a new value
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      //Here we return all the previous items + the new inputText value
+      //add it to the empty array
+      return [...prevItems, inputText];
+    });
+    //to empty the input area
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item) => {
+            return <li>{item}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
